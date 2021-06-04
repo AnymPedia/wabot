be8f011d2fef28bbd8bbe26513343bb2792e30e2
Anym <68510045+AnymPedia@users.noreply.github.com>
2021-06-04 09:00:35 +0700

    Update broadcastgroups.js

--- plugins/broadcastgroups.js
+++ plugins/broadcastgroups.js
@@ -1,8 +1,9 @@
-let handler  = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, participants, isOwner }) => {
+  let users = participants.map(u => u.jid)
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us') && !v.read_only && v.message && !v.announce).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
-  let content = conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 All Group Broadcast 」')
+  let content = conn.reply(id, `*「 Broadcast 」*\n\n${text}`, null, { contextInfo: { mentionedJid: users } })
   for (let id of groups) conn.copyNForward(id, content, true)
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${groups.length} grup_`, m)
 }
